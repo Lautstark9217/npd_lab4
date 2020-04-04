6a6fa8990ea6c744f5d027cd41d6b16c4eb51385
Calvin <naws_9217@outlook.com>
2020-04-04 11:57:19 +0800

    day1

--- client.h
+++ client.h
@@ -1,16 +1,29 @@
 #ifndef LAB4CLIENT
 #define LAB4CLIENT
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/epoll.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "common.h"
+struct Client {
+    // connect to a server
+    void clientConnect();
+    // close connection
+    void clientClose();
+    // start a client program
+    void clientStart();
+    // current sock fd
+    int sockfd;
+    // current thread id
+    int pid;
+    // epoll_create return
+    int epfd;
+    // pipe connect
+    int pipe_fd[2];
+    //whether online
+    bool isClientwork;
+    //whether in duel
+    bool isClientinDuel;
+    Msg msg;
+    char send_buf[BUF_SIZE];
+    char recv_buf[BUF_SIZE];
+    struct sockaddr_in serverAddr;
+};
 
 #endif
--- common.h
+++ common.h
@@ -1,4 +1,36 @@
 #ifndef LAB4COMMON
 #define LAB4COMMON
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 7788
+#define EPOLL_SIZE 5000
+#define BUF_SIZE 0xFFFF
+#define SERVER_WELCOME "Welcome you join to the game! Your ID is: %s\n"
+#define SERVER_MULTI_ID "This ID has been used by another user.\n"
+#define SERVER_LEAVE "User %s has left the game.\n"
+#define CLIENT_HP 100
+#define CLIENT_DMG 40
+
+struct Msg
+{
+    int type;
+    char fromID[10];
+    char toID[10];
+    char content[BUF_SIZE];
+};
+static void addfd( int epollfd, int fd);
+
 #endif
